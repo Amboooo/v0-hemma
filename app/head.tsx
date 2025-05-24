3ae392cf587135decf18f19c739d28a81cfae40c
Amboooo <104835714+Amboooo@users.noreply.github.com>
2025-05-24 13:24:57 +0200

    Update head.tsx with manifest and icons

--- app/head.tsx
+++ app/head.tsx
@@ -6,10 +6,12 @@ export default function Head() {
       <meta name="theme-color" content="#000000" />
       <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
       <link rel="manifest" href="/manifest.json" />
+      <link rel="icon" href="/icons/icon-192x192.png" />
       <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192x192.png" />
       <link rel="apple-touch-icon" sizes="512x512" href="/icons/icon-512x512.png" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      <meta name="mobile-web-app-capable" content="yes" />
     </>
   )
 }
