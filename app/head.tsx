8cdf35be716dee0f8615e2a2848c94a516902d4d
Amboooo <104835714+Amboooo@users.noreply.github.com>
2025-05-24 13:53:35 +0200

    Add next-pwa setup

--- app/head.tsx
+++ app/head.tsx
@@ -4,9 +4,8 @@ export default function Head() {
       <title>Hemmafix</title>
       <meta name="application-name" content="Hemmafix" />
       <meta name="theme-color" content="#000000" />
-      <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
       <link rel="manifest" href="/manifest.json" />
-      <link rel="icon" href="/icons/icon-192x192.png" />
+      <link rel="icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192x192.png" />
       <link rel="apple-touch-icon" sizes="512x512" href="/icons/icon-512x512.png" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
--- next.config.js
+++ next.config.js
@@ -2,12 +2,14 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // disable i dev-mode
 })
 
 const nextConfig = withPWA({
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
 })
 
 module.exports = nextConfig
